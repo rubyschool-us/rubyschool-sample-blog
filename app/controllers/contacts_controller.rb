1ab8c219f998498a9100d206196b6be5a51c8a65
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-16 23:10:14 +0400

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,14 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 
 end
