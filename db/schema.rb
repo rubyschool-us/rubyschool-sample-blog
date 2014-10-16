706bd85f88c5161bdb8dd226f4f638af57a70ff0
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-16 22:40:59 +0400

    Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014190230) do
+ActiveRecord::Schema.define(version: 20141016183232) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "email"
+    t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
