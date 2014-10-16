35fc4f6202571cb88613de3ad67fd08c6e67b13b
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-16 23:21:15 +0400

    Add contact model validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,11 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    @contact.save
+    if @contact.valid?
+      @contact.save
+    else
+      render action: 'new'
+    end
   end
 
   private
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ActiveRecord::Base
+  validates :email, presence: true
+  validates :message, presence: true
 end
