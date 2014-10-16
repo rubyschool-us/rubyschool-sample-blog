7e4af25c7bae0c8245e58822c6d29421a6069d3c
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-16 23:29:10 +0400

    Change /contacts/new -> /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new, :create] 
+  resource :contacts, only: [:new, :create], path_names: { :new => '' } 
   resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
