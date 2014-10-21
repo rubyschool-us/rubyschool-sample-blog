5abd1e334861631a5b48a7baeb8889178150e91b
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-21 23:35:54 +0400

    Add terms and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def terms
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   resource :contacts, only: [:new, :create], path_names: { :new => '' } 
   resources :articles
 
