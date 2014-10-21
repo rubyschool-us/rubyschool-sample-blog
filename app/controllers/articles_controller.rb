3d4150f689d6677cd91a5d7025d89151ce3ec37b
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-21 23:26:19 +0400

    Implement articles#edit articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end    
   end
 
   private
