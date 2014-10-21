d89ccaa186e1d824cde0943773de722068e25a62
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-21 22:26:30 +0400

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
