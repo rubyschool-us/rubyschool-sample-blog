ddf2e5d02e6fc584bf75570beb5772c8234dfa6e
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-18 23:44:10 +0300

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,16 @@ FactoryGirl.define do
   factory :article do
     title "Article title"
     text "Article text"
+
+    # создаем фабрику с названием article_with_comments
+    # для создания статьи с несколькими комментариями
+    factory :article_with_comments do
+      # после создания article
+      after :create do |article, evaluator|
+        # создаем список из 3-х комментариев
+        create_list :comment, 3, article: article
+      end
+    end
+
   end
 end
