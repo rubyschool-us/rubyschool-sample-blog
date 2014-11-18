17b70f23a1c0b128414969f696869a81122c7ec2
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-18 23:22:57 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,14 @@ describe Article do
   describe "associations" do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end
