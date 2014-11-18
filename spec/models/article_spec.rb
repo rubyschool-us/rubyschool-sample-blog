b9b3eeb3cb45cad816eca1ba022501174acea06a
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-18 23:44:24 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -19,4 +19,14 @@ describe Article do
       expect(article.subject).to eq 'Lorem Ipsum'
     end
   end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      # создаем статью, но в этот раз с комментариями
+      article = create(:article_with_comments)
+
+      # проверка
+      expect(article.last_comment.body).to eq "comment body 3"
+    end
+  end
 end
