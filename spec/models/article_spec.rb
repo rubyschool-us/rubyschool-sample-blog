f373552ddeefe9a2bd9762a94ca4721f831e449c
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-18 22:42:43 +0300

    Update article spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Article do
-  it { should have_many :comments }
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
 end
